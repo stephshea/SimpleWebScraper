2d9a1b601768fe10c3e444dfbdb2a51e7de72a1b
stephshea <praise1God!>
2018-05-31 15:57:44 -0400

    initial commit

---
+++ SimpleWebScraper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleWebScraper
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
